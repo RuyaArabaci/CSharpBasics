b8e19a1a96c3bff3c49ceb3fa1f9337f13d7a3ff
Rüya Arabacı <ruyaarabaci2021@gmail.com>
2024-02-18 17:47:58 +0300

    Proje dosyası ekle.

---
+++ NumberBasics/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSharpBasics
+{
+    public class Program()
+    {
+        static void Main(string[] args)
+        {
+            // Some number types are intager, float, double, decimal.
+
+        }
+    }
+}
---
+++ StringBasics/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CSharpBasics
+{
+    public class Program()
+    {
+        static void Main(string[] args)
+        {
+            string studentName = "Cihat";
+            string studentSurname = "Çetin";
+            string studentAge = "17";
+
+            Console.WriteLine(studentName);
+            Console.WriteLine($"{studentName} {studentSurname}");
+            Console.WriteLine($"Student name is {studentName}.");
+            Console.WriteLine($"Student name is {studentName} and age is {studentAge}.");
+            // We can use $ and {} signs for write clear codes.
+            // WARNING!! We can just use srting values.
+
+            Console.WriteLine($"Student name's length is {studentName.Length} and age's length is {studentAge.Length}.");
+            // Length tool is show us the length of the variables.
+
+            Console.WriteLine($"{studentName.ToLower()} {studentSurname.ToUpper()}");
+            // ToLower() converts all the letters lowercase and ToUpper() converts all the letters uppercase.
+
+            Console.WriteLine($"{studentName.Replace("Cihat" , "Cihan")} {studentSurname.Replace("t", "l")}");
+            // We can use Replace(x,y) tool for change all the x variables to y. But it is not constant changing.
+
+            Console.WriteLine($"{studentName.Contains("h")} {studentSurname.Contains("h")}");
+            // Contains() tool helps us relize whether it is present or not.
+
+            Console.ReadLine();
+        }
+    }
+}
